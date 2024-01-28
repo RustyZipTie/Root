5245ff44ebd36e76e5d6e74a2ba814a772c60e9e
RustyZipTie <kaigrignon@gmail.com>
2024-01-28 07:56:30 -0800

    in proscess of restructuring to OO. compiler is not currently functional. also added skeleton website

--- main.js
+++ main.js
@@ -2,7 +2,117 @@ const fs = require('fs');
 const { delimiter } = require('path');
 const stream = require('stream');
 
-//categorizing things into objects for organizational purposes...
+//classes
+class Code extends String {
+    
+    toElems(){
+        const elems = [];
+        for(let chunk of this.trim().split()){
+            chunk = chunk.toCode();
+            elems.push(new Elem(chunk));
+        }
+        return elems;
+    }
+}
+class Clump{
+    constructor(code) {
+
+    }
+
+    addElem(elem) {
+        let success = false;
+        if (!this.elems.find(val => val.id === elem.id)) {
+            this.elems.push(elem);
+            success = true;
+        }
+        return success;
+    }
+
+    getByIndex(idx) {
+        return this.elems[idx];
+    }
+
+    getById(id) {
+        for (elem of this.elems) {
+            if (elem.id === id) {
+                return elem;
+            }
+        }
+        return undefined;
+    }
+}
+
+class Act{
+    constructor(code){
+
+    }
+
+    execute(){
+
+    }
+}
+
+class Elem {
+
+    constructor(code) {
+        this.id = code.trim().splitFirstRemove(':')[0];
+        this.code = code.rim().splitFirstRemove(':')[1];
+        this.parse();
+        this.decideType();
+    }
+
+    parse(){
+        this.decideType();
+        switch(this.type){
+            case 'clump':
+                this.content = new Clump(this.code);
+                break;
+
+            case 'act':
+                this.content = new Act(this.code);
+                break;
+
+            case 'boolean':
+                this.content = (this.code === 'true');
+                break;
+
+            case 'string':
+                this.content = this.code;
+                break;
+
+            case 'number':
+                this.content = Number(this.code);
+                break;
+        }
+    }
+
+
+    decideType(code){
+        const nums = [...'0123456789'];
+        switch (code.charAt(0)) {
+            case '[':
+                this.type = 'clump';
+            case '\\':
+            case '{':
+                this.type = 'act';
+            case 't':
+            case 'f':
+                this.type = (code.trim() === 'true' || code.trim() === 'false') ? 'boolean' : undefined;
+            case '`':
+            case "'":
+            case '"':
+                this.type = 'string';
+            default:
+                let num = true;
+                for (char of [...code]) {
+                    num = nums.includes(char);
+                }
+                this.type = num ? 'number' : undefined;
+        }
+    }
+}
+
+
 const io = {
     getCode: fileName => {
         fs.readFile(fileName, (err, data) => {
@@ -16,138 +126,122 @@ const io = {
     }
 }
 
-/**
- * This object contains methods for parsing text
- */
-const wordPlay = {
 
-    /**
-     * splits string to array
-     * split happens to the right of the delimiter
-     * e.g. splitRight('gone', 'o') -> ['go','ne]
-     * @param {string} code 
-     * @param {string} delimiter 
-     * @returns {string[]}
-     */
-    splitRight: (code, delimiter) => {
-        const chars = [...code];
-        let section = [];
-        let sections = []
-        for (let char of chars) {
+
+
+//add my custom methods to String...
+String.prototype.splitRight = function(delimiter) {
+    const chars = [...this];
+    let section = [];
+    let sections = []
+    for (let char of chars) {
+        section.push(char);
+        if (char === delimiter) {
+            sections.push(section.join(''));
+            section = [];
+        }
+    }
+    sections.push(section.join(''));
+    return sections;
+}
+
+String.prototype.splitLeft = function(delimiter){
+    const chars = [...this];
+    let section = [];
+    let sections = []
+    for (let char of chars) {
+        if (char === delimiter) {
+            sections.push(section ? section.join('') : 'bla');
+            section = [];
+        }
+        section.push(char);
+
+    }
+    sections.push(section.join(''));
+    return sections;
+}
+
+String.prototype.splitBoth = function(delimiter) {
+    const chars = [...this];
+    let section = [];
+    let sections = []
+    for (let char of chars) {
+        if (char === delimiter) {
+            sections.push(section.join(''));
+            sections.push(char);
+            section = [];
+        } else {
             section.push(char);
-            if (char === delimiter) {
-                sections.push(section.join(''));
-                section = [];
-            }
         }
-        sections.push(section.join(''));
-        return sections;
-    },
+    }
+    sections.push(section.join(''));
+    return sections;
+}
 
-    /**
-     * splits to left of delimiter
-     * e.g. splitLeft('gone', 'o') -> ['g','one']
-     * @param {string} code 
-     * @param {string} delimiter 
-     * @returns {string[]}
-     */
-    splitLeft: (code, delimiter) => {
-        const chars = [...code];
-        let section = [];
-        let sections = []
-        for (let char of chars) {
-            if (char === delimiter) {
-                sections.push(section ? section.join('') : 'bla');
-                section = [];
-            }
+String.prototype.splitRightMulti = function(delimiters) {
+    const chars = [...this];
+    let section = [];
+    let sections = []
+    for (let char of chars) {
+        section.push(char);
+        if (delimiters.includes(char)) {
+            sections.push(section.join(''));
+            section = [];
+        }
+    }
+    sections.push(section.join(''));
+    return sections;
+}
+
+String.prototype.splitLeftMulti = function(delimiters) {
+    const chars = [...this];
+    let section = [];
+    let sections = []
+    for (let char of chars) {
+        if (delimiters.includes(char)) {
+            sections.push(section ? section.join('') : 'bla');
+            section = [];
+        }
+        section.push(char);
+
+    }
+    sections.push(section.join(''));
+    return sections;
+}
+
+String.prototype.splitBothMulti = function(delimiters) {
+    const chars = [...this];
+    let section = [];
+    let sections = []
+    for (let char of chars) {
+        if (delimiters.includes(char)) {
+            sections.push(section.join(''));
+            sections.push(char);
+            section = [];
+        } else {
             section.push(char);
-
         }
-        sections.push(section.join(''));
-        return sections;
-    },
+    }
+    sections.push(section.join(''));
+    return sections;
+}
 
-    splitBoth: (code, delimiter) => {
-        const chars = [...code];
-        let section = [];
-        let sections = []
-        for (let char of chars) {
-            if (char === delimiter) {
-                sections.push(section.join(''));
-                sections.push(char);
-                section = [];
-            } else {
-                section.push(char);
-            }
-        }
-        sections.push(section.join(''));
-        return sections;
-    },
+String.prototype.splitFirstRemove = function(delimiter) {
+    let first = '';
+    let second = '';
+    const chars = [...this];
+    let i = 0;
+    for (; chars[i] !== delimiter; i++) {
+        first = first.concat(chars[i]);
+    }
+    i++;
+    for (; i < chars.length; i++) {
+        second = second.concat(chars[i]);
+    }
+    return [first.trim(), second.trim()];
+},
 
-    splitRightMulti: (code, delimiters) => {
-        const chars = [...code];
-        let section = [];
-        let sections = []
-        for (let char of chars) {
-            section.push(char);
-            if (delimiters.includes(char)) {
-                sections.push(section.join(''));
-                section = [];
-            }
-        }
-        sections.push(section.join(''));
-        return sections;
-    },
-
-    splitLeftMulti: (code, delimiters) => {
-        const chars = [...code];
-        let section = [];
-        let sections = []
-        for (let char of chars) {
-            if (delimiters.includes(char)) {
-                sections.push(section ? section.join('') : 'bla');
-                section = [];
-            }
-            section.push(char);
-
-        }
-        sections.push(section.join(''));
-        return sections;
-    },
-
-    splitBothMulti: (code, delimiters) => {
-        const chars = [...code];
-        let section = [];
-        let sections = []
-        for (let char of chars) {
-            if (delimiters.includes(char)) {
-                sections.push(section.join(''));
-                sections.push(char);
-                section = [];
-            } else {
-                section.push(char);
-            }
-        }
-        sections.push(section.join(''));
-        return sections;
-    },
-
-    splitFirstRemove: (code, delimiter) => {
-        let first = '';
-        let second = '';
-        const chars = [...code];
-        let i = 0;
-        for(;chars[i] !== delimiter; i++){
-            first = first.concat(chars[i]);
-        }
-        i++;
-        for(;i<chars.length; i++){
-            second = second.concat(chars[i]);
-        }
-        return [first.trim(), second.trim()];
-    },
-
-    getBetween: (code, start, end) => {
+String.prototype.getBetween = function(start, end) {
         if (start === end) {
             //console.log('simple');
             return split[split.find(val => val === start) + 1];
@@ -156,9 +250,9 @@ const wordPlay = {
             let indent = 0;
             let content = '';
             let started = false;
-            const split = wordPlay.splitBothMulti(code, [start, end]);
+            const split = code.splitBothMulti([start, end]);
             for (let section of split) {
-                
+
                 section = section.trim();
                 switch (section) {
                     case start:
@@ -181,7 +275,10 @@ const wordPlay = {
             }
             return content;
         }
-    }
+}
+
+String.prototype.toCode = function(){
+    return new Code(this);
 }
 
 const codePlay = {
@@ -218,13 +315,13 @@ const codePlay = {
     },
 
     parseToElements: code => {
-        const raws = wordPlay.getBetween(code, '[', ']').split(',');
+        const raws = code.getBetween('[', ']').split(',');
         //console.log('raws:',raws);
-        const clumpObj = { name: wordPlay.splitLeftMulti(code, [' ', '['])[0], elems: [] };
+        const clumpObj = { name: code.splitLeftMulti([' ', '['])[0], elems: [] };
         for (const raw of raws) {
             clumpObj.elems.push({
-                id: wordPlay.splitFirstRemove(raw, ':')[0].trim(),
-                code: wordPlay.splitFirstRemove(raw, ':')[1].trim(),
+                id: raw.splitFirstRemove(':')[0].trim(),
+                code: raw.splitFirstRemove(':')[1].trim(),
                 type: codePlay.decideType(raw.split(':')[1].trim())
             });
         }
@@ -259,9 +356,13 @@ const codePlay = {
 
 
 function go(code) {
-    let clumps = codePlay.parseToClumps(code);
-    console.log(clumps[0].name, clumps[0].elems);
-    console.log(clumps[1].name, clumps[1].elems);
+    // let clumps = codePlay.parseToClumps(code);
+    // console.log(clumps[0].name, clumps[0].elems);
+    // console.log(clumps[1].name, clumps[1].elems);
+
+    const grib = 'grib';
+    grib.bla = 10;
+    console.log(grib.bla);
     //more stuff...
 }
 
